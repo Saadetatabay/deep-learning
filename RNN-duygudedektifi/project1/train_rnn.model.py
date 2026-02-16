2e35010bb72ee7d787fe977731994942c8ee7a6b
Saadetatabay <etatabaysaad143@gmail.com>
2026-02-16 22:19:55 +0300

    Add decoding functionality for IMDB dataset reviews

--- RNN-duygudedektifi/project1/train_rnn.model.py
+++ RNN-duygudedektifi/project1/train_rnn.model.py
@@ -11,5 +11,15 @@ nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
 # Load the IMDB dataset
+# x_train, y_train: eğitim verisi ve etiketleri
 max_features = 10000  # en sık kullanılan 10.000 kelime
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features) # sayısallaştırılmış veriyi yükle
+
+# sayıları kelimelere dönüştür
+word_index = imdb.get_word_index() # kelime indeksini al "the":1 
+index_word = {v: k for k, v in word_index.items()} # indeks kelimeye dönüştür "1":"the"
+
+# 3 çıkarmamızın sebebi, 0, 1, 2'nin özel karakterler için ayrılmış olmasıdır.
+decoded = " ".join([index_word.get(i-3,"?") for i in x_train[0]])
+
+print("Decoded review:", decoded)
