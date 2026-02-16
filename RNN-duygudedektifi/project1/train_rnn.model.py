b3866e91d4e7fb38bd49708104ada0c2ddca27f0
Saadetatabay <etatabaysaad143@gmail.com>
2026-02-16 21:56:39 +0300

    Implement data loading and preprocessing for IMDB dataset in RNN model

--- RNN-duygudedektifi/project1/train_rnn.model.py
+++ RNN-duygudedektifi/project1/train_rnn.model.py
@@ -5,3 +5,11 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, SimpleRNN, Dense
 from tensorflow.keras.datasets import imdb
+
+# stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+# Load the IMDB dataset
+max_features = 10000  # en sık kullanılan 10.000 kelime
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
